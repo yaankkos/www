f3f2d475c03c0a82631d3b10272376158ca46204
yana <nuymaftp@prof.ru>
2026-03-04 23:18:24 -0800

    Количество элементов

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,23 +3,9 @@
 	<head>
 		<title>my view</title>
 	</head>
-	<!-- индекс текущей итерации -->
-<!-- @foreach ($arr as $elem)
-	{{ $loop->index }}
-	{{ $elem }}
-@endforeach -->
-
-<!-- номер текущей операции -->
-<!-- @foreach ($arr as $elem)
-	{{ $loop->iteration }}
-	{{ $elem }}
-@endforeach -->
-
-<!-- скольк итераций осталось -->
- @foreach ($arr as $elem)
-	{{ $loop->remaining }}
+@foreach ($arr as $elem)
+	{{ $loop->count }}
 	{{ $elem }}
 @endforeach
-
 	</body>
 </html>
