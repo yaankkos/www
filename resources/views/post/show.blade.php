3816bf40fac3429676e06bd07137b87a814a5d4f
yana <nuymaftp@prof.ru>
2026-03-04 23:21:17 -0800

    Директива @break

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -3,9 +3,21 @@
 	<head>
 		<title>my view</title>
 	</head>
-@foreach ($arr as $elem)
-	{{ $loop->count }}
+	<!-- Можно завершать работу цикла с помощью директивы break -->
+<!-- @foreach ($arr as $elem)
 	{{ $elem }}
+	
+	@if ($elem == 3)
+		@break
+	@endif
+@endforeach -->
+
+<!-- Можно включить условие в строку объявления директивы: -->
+@foreach ($arr as $elem)
+	{{ $elem }}
+	
+	@break($elem == 3)
 @endforeach
+
 	</body>
 </html>
