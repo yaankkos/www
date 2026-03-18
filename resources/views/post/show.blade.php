8b631f68d0db55132c554891556e4d5bc2280e9d
yana <nuymaftp@prof.ru>
2026-03-17 20:31:14 -0700

    Задание 17

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,13 +5,10 @@
 	<title>my view</title>
 </head>
 <body>
-@if($age>18)
-<p>Вам больше 18 лет.</p>
-@elseif($age==18)
-<p>Вам ровно 18 лет.</p>
-@else 
-<p>Вам меньше 18 лет.</p>
-@endif
+@unless ($age>18)
+	<p>Вам еще нет 18 лет</p>
+@endunless
+
 </body>
 
 </html>
