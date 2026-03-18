4b9ab78af5f38da68097ed3a304a21079240d82a
yana <nuymaftp@prof.ru>
2026-03-17 21:36:28 -0700

    Задание 26

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,7 +6,7 @@ class PostController extends Controller
 	{
 		public function show()
 		{
-			return view('post.show', ['data' => '4']);
+			return view('post.show', ['data' => [1, 2, 3], [4, 5, 6], [7, 8, 9]]);
 	}
 	}
 
--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -5,17 +5,11 @@
 	<title>my view</title>
 </head>
 <body>
-@if(is_array($data))
-<p>Это массив</p>
-<ul>
-	@foreach($data as $item)
-	<li>{{ $item }}</li>
-	@endForeach
-</ul>	
-@else
-<p>Это число:</p>
-	<p>{{ $data }}</p>
-@endif
+<table border="1">
+	@foreach($data as $cell)
+	<td>{{ $cell }}</td>
+	@endforeach
+</table>
 </body>
 
 </html>
