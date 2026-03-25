94402d4de02657c3058c3a666c2210441a72761e
yana <nuymaftp@prof.ru>
2026-03-24 23:37:45 -0700

    проверка

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,15 +5,24 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-Collection::macro('toLocale', function ($locale) {
-    return $this->map(function ($value) use ($locale) {
-        return Lang::get($value, [], $locale);
-    });
-});
+// Collection::macro('toLocale', function ($locale) {
+//     return $this->map(function ($value) use ($locale) {
+//         return Lang::get($value, [], $locale);
+//     });
+// });
 
-$collection = collect(['first', 'second']);
+// $collection = collect(['first', 'second']);
 
-$translated = $collection->toLocale('es');
-dd($translated);
-	
+// $translated = $collection->toLocale('es');
+
+$average = collect([
+    ['foo' => 10],
+    ['foo' => 10],
+    ['foo' => 20],
+    ['foo' => 40]
+])->avg('foo');
+
+
+$average = collect([1, 1, 2, 4])->avg();
+dd($average);
 ?>
