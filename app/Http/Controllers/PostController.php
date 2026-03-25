6ae276e568bc7b59613cc4f15c62089005b1494c
yana <nuymaftp@prof.ru>
2026-03-25 00:31:23 -0700

    countBy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-$collection = collect([1, 2, 3, 4]);
+$collection = collect([1, 2, 2, 2, 3]);
 
-$collection->count();
-dd($collection);
+$counted = $collection->countBy();
+
+$counted->all();
+dd($counted);
 ?>
