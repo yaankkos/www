6a2bec333790c3cb0eebef6bbd582e5f2c9c6f6f
yana <nuymaftp@prof.ru>
2026-03-25 00:07:05 -0700

    avq

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,16 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-// Collection::macro('toLocale', function ($locale) {
-//     return $this->map(function ($value) use ($locale) {
-//         return Lang::get($value, [], $locale);
-//     });
-// });
-
-// $collection = collect(['first', 'second']);
-
-// $translated = $collection->toLocale('es');
-
 $average = collect([
     ['foo' => 10],
     ['foo' => 10],
