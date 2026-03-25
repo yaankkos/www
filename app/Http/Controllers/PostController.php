acde7a44a6318508184d678fcfcdfb65b6a8f7bd
yana <nuymaftp@prof.ru>
2026-03-25 00:11:03 -0700

    chunk

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,14 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-$average = collect([
-    ['foo' => 10],
-    ['foo' => 10],
-    ['foo' => 20],
-    ['foo' => 40]
-])->avg('foo');
+$collection = collect([1, 2, 3, 4, 5, 6, 7]);
 
+$chunks = $collection->chunk(4);
 
-$average = collect([1, 1, 2, 4])->avg();
-dd($average);
+$chunks->all();
+dd($chunks);
 ?>
