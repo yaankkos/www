4d2f5b0742cff9aa2c15e5c094858ffc966d1011
yana <nuymaftp@prof.ru>
2026-03-26 02:07:30 -0700

    intersect

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-$collection = collect([
-    ['account_id' => 1, 'product' => 'Desk'],
-    ['account_id' => 2, 'product' => 'Chair'],
-]);
+$collection = collect(['Desk', 'Sofa', 'Chair']);
 
-$collection->implode('product', ', ');
-dd($collection);
+$intersect = $collection->intersect(['Desk', 'Chair', 'Bookcase']);
+
+$intersect->all();
+dd($intersect);
 ?>
