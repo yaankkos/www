cc7433f881a08d380f5dc73aff3975cffc0c7b7d
yana <nuymaftp@prof.ru>
2026-03-26 02:26:54 -0700

    partition

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,11 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-$collection = collect(['A', 'B', 'C']);
+$collection = collect([1, 2, 3, 4, 5, 6]);
 
-$filtered = $collection->pad(5, 0);
-$filtered = $collection->pad(-5, 0);
-
-$filtered->all();
-dd($filtered);
+[$underThree, $equalOrAboveThree] = $collection->partition(function ($i) {
+    return $i < 3;
+});
+$equalOrAboveThree->all();
+dd($collection);
 ?>
