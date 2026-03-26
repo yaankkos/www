2ad9e1a8da0707d2c8d61a2a6054004efd0a15e1
yana <nuymaftp@prof.ru>
2026-03-26 02:03:03 -0700

    groupBy

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,8 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-$collection = collect(['name' => 'taylor', 'framework' => 'laravel']);
+$collection = collect([
+    ['account_id' => 'account-x10', 'product' => 'Chair'],
+    ['account_id' => 'account-x10', 'product' => 'Bookcase'],
+    ['account_id' => 'account-x11', 'product' => 'Desk'],
+]);
 
-$value = $collection->get('name');
-dd($value);
+$grouped = $collection->groupBy('account_id');
+
+$grouped->all();
+dd($grouped);
 ?>
