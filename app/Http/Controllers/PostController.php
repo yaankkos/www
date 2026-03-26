49cc7620984035dd5af86cf8016811b9c2d0b97f
yana <nuymaftp@prof.ru>
2026-03-26 01:34:38 -0700

    duplicates

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,9 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-$collection = collect(['John Doe', 'Jane Doe']);
+$collection = collect(['a', 'b', 'a', 'c', 'b']);
 
-$collection->dump();
+$collection->duplicates();
 
 dd($collection);
 ?>
