5c5e0250ca99ff54f6a9213f0351766af77f0350
yana <nuymaftp@prof.ru>
2026-03-26 02:01:07 -0700

    get

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,10 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-$collection = collect([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+$collection = collect(['name' => 'taylor', 'framework' => 'laravel']);
 
-$chunk = $collection->forPage(2, 3);
-
-$chunk->all();
-dd($chunk);
+$value = $collection->get('name');
+dd($value);
 ?>
