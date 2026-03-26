f125f4f72f0e022078d8ad6aec30b573512fc0f3
yana <nuymaftp@prof.ru>
2026-03-26 02:24:05 -0700

    nth

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,14 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-$mode = collect([
-    ['foo' => 10],
-    ['foo' => 10],
-    ['foo' => 20],
-    ['foo' => 40]
-])->mode('foo');
+$collection = collect(['a', 'b', 'c', 'd', 'e', 'f']);
 
-$mode = collect([1, 1, 2, 4])->mode();
-$mode = collect([1, 1, 2, 2])->mode();
-dd($mode);
+$collection->nth(4);
+dd($collection);
 ?>
