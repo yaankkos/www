a152844da95a9078d6f710c79a46b5851f210aab
yana <nuymaftp@prof.ru>
2026-03-26 02:22:58 -0700

    mode

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,7 +5,14 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-$min = collect([['foo' => 10], ['foo' => 20]])->min('foo');
-$min = collect([1, 2, 3, 4, 5])->min();
-dd($min);
+$mode = collect([
+    ['foo' => 10],
+    ['foo' => 10],
+    ['foo' => 20],
+    ['foo' => 40]
+])->mode('foo');
+
+$mode = collect([1, 1, 2, 4])->mode();
+$mode = collect([1, 1, 2, 2])->mode();
+dd($mode);
 ?>
