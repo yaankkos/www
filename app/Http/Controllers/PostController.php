ec7cc8f8d56df43aed9d2b73ed90c18ec9ab601e
yana <nuymaftp@prof.ru>
2026-03-26 01:37:53 -0700

    every

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,9 +5,11 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-$collection = collect(['a', 'b', 'a', 'c', 'b']);
+$collection = collect([]);
 
-$collection->duplicates();
+$collection->every(function ($value, $key) {
+    return $value > 2;
+});
 
 dd($collection);
 ?>
