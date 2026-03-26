de6025be7ecd2778c0e7d6601fc83b83ea252543
yana <nuymaftp@prof.ru>
2026-03-26 02:29:15 -0700

    pipeThrough

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,29 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-class ResourceCollection
-{
-    /**
-     * Экземпляр Collection.
-     */
-    public $collection;
-
-    /**
-     * Создать новый экземпляр ResourceCollection.
-     *
-     * @param  Collection  $collection
-     * @return void
-     */
-    public function __construct(Collection $collection)
-    {
-        $this->collection = $collection;
-    }
-}
-
 $collection = collect([1, 2, 3]);
 
-$resource = $collection->pipeInto(ResourceCollection::class);
-
-$resource->collection->all();
-dd($resource);
+$result = $collection->pipeThrough([
+    function ($collection) {
+        return $collection->merge([4, 5]);
+    },
+    function ($collection) {
+        return $collection->sum();
+    },
+]);
+dd($result);
 ?>
