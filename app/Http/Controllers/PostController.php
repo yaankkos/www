be0c08b3ff5febbda394ef52b5623c614c23c546
yana <nuymaftp@prof.ru>
2026-03-26 02:16:19 -0700

    mapToGroups

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,14 +5,35 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Lang;
 
-$collection = collect([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+$collection = collect([
+    [
+        'name' => 'John Doe',
+        'department' => 'Sales',
+    ],
+    [
+        'name' => 'Jane Doe',
+        'department' => 'Sales',
+    ],
+    [
+        'name' => 'Johnny Doe',
+        'department' => 'Marketing',
+    ]
+]);
 
-$chunks = $collection->chunk(2);
-
-$sequence = $chunks->mapSpread(function ($even, $odd) {
-    return $even + $odd;
+$grouped = $collection->mapToGroups(function ($item, $key) {
+    return [$item['department'] => $item['name']];
 });
 
-$sequence->all();
-dd($sequence);
+$grouped->all();
+
+/*
+    [
+        'Sales' => ['John Doe', 'Jane Doe'],
+        'Marketing' => ['Johnny Doe'],
+    ]
+*/
+
+$grouped->get('Sales')->all();
+
+dd($grouped);
 ?>
