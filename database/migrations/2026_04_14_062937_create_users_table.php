7541e70a564baf4e9383e20828d3f8ebf5009b0b
yana <nuymaftp@prof.ru>
2026-04-13 23:31:09 -0700

    задача5

--- database/migrations/2026_04_14_062937_create_users_table.php
+++ database/migrations/2026_04_14_062937_create_users_table.php
@@ -6,21 +6,19 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->string('name');             
+            $table->string('surname');          
+            $table->date('birth_date');         
+            $table->date('user_created_date');  
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('users');
     }
