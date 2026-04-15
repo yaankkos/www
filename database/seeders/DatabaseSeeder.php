e190367dc0017619905ba1f3b416ecb9fe58d469
yana <nuymaftp@prof.ru>
2026-04-15 03:26:26 -0700

    генерация случайных строк в сидерах

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,27 +6,16 @@ use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
         DB::table('posts')->insert([
-            [
-                'title' => 'title 1',
-                'slug'  => 'post-1',
-                'text'  => 'text text text 1',
-            ],
-            [
-                'title' => 'title 2',
-                'slug'  => 'post-2',
-                'text'  => 'text text text 2',
-            ],
-            [
-                'title' => 'title 3',
-                'slug'  => 'post-3',
-                'text'  => 'text text text 3',
-            ],
+            'title' => Str::random(10),
+            'slug'  => Str::random(10),
+            'text'  => Str::random(50),
         ]);
     }
 }
