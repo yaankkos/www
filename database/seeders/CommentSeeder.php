62bd70f0611afa496beea1d594e14eca034052c2
yana <nuymaftp@prof.ru>
2026-04-15 03:42:54 -0700

    общий вызов отдельных сидеров

---
+++ database/seeders/CommentSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class CommentSeeder extends Seeder
+{
+    public function run() {}
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,20 +9,15 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Hash;
 
+
+
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => 'pZRLTZ0mWX',
-            'email' => 'ITBQX0Kr4d@gmail.com',
-            'password' => '$2y$12$/U7YCvuesubfxyF.pYsxxONvnlZLsOxuzKXQXdGSpkFtcT4jCQvYa',
-            'surname' => 'Default Surname',
-            'birth_date' => '2000-01-01',
-            'age' => 25,  // Добавьте это поле
-            'user_created_date' => now(),
-            'created_at' => now(),
-            'updated_at' => now(),
+        $this->call([
+            PostSeeder::class,
+            CommentSeeder::class,
         ]);
     }
 }
