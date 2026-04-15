3d4523fe704d922244b414f64a559a0f39c322d8
yana <nuymaftp@prof.ru>
2026-04-15 03:35:54 -0700

    хеширование паролей в сидерах

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -7,13 +7,22 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        DB::table('domains')->insert([
-            'name' => Str::random(5) . '.' . Str::random(10) . '.com',
+        DB::table('users')->insert([
+            'name' => 'pZRLTZ0mWX',
+            'email' => 'ITBQX0Kr4d@gmail.com',
+            'password' => '$2y$12$/U7YCvuesubfxyF.pYsxxONvnlZLsOxuzKXQXdGSpkFtcT4jCQvYa',
+            'surname' => 'Default Surname',
+            'birth_date' => '2000-01-01',
+            'age' => 25,  // Добавьте это поле
+            'user_created_date' => now(),
+            'created_at' => now(),
+            'updated_at' => now(),
         ]);
     }
 }
