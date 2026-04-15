4dac029f98504a2d809f56353395a9a26e510c4e
yana <nuymaftp@prof.ru>
2026-04-15 03:12:50 -0700

    вставили в таблицу с постами новый пост

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,21 +5,28 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
-    /**
-     * Seed the application's database.
-     */
-    public function run(): void
+    public function run()
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('posts')->insert([
+            [
+                'title' => 'title 1',
+                'slug'  => 'post-1',
+                'text'  => 'text text text 1',
+            ],
+            [
+                'title' => 'title 2',
+                'slug'  => 'post-2',
+                'text'  => 'text text text 2',
+            ],
+            [
+                'title' => 'title 3',
+                'slug'  => 'post-3',
+                'text'  => 'text text text 3',
+            ],
         ]);
     }
 }
