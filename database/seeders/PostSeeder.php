34b6238a5aab2e78b7feaceaba9f840fb53509fc
yana <nuymaftp@prof.ru>
2026-04-15 03:38:28 -0700

    отдельные классы сидеров

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class PostSeeder extends Seeder
+{
+    public function run() {}
+}
