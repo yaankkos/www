2b98b04fb71dd5ed8b4d946f0d64beecd4cb7ddf
yana <nuymaftp@prof.ru>
2026-04-15 23:33:41 -0700

    несколько условий where при выборке через QB

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -10,6 +10,9 @@ class PostController extends Controller
 	{
 		public function show()
 		{
-            $posts = DB::table('posts')->where('likes', '!=', 100)->get();
+            $posts = DB::table('posts')
+		->where('likes', '>', 10)
+		->where('likes', '<', 20)
+		->get();
 		}
 	}
