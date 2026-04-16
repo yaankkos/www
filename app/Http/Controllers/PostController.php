9181f4bf5d715d3ee9f9e5889d08d8d501923441
yana <nuymaftp@prof.ru>
2026-04-16 00:05:58 -0700

    количество записей при выборке через QB

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,10 +11,12 @@ class PostController extends Controller
     public function show()
     {
         $posts = DB::table('posts')
-            ->inRandomOrder()
+            ->take(5)
             ->get();
 
         dump($posts);
+
+
 
 
 
