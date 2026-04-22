02a2bb430935cfc46930fb990e10b4a20e16fbbd
yana <nuymaftp@prof.ru>
2026-04-22 03:26:36 -0700

    Задача 54

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,7 +8,7 @@ class PostController extends Controller
 {
     public function show()
     {
-       DB::table('users')->where('age', 30)->increment('salary', 100);
-        return "Зарплата увеличена на 100 для пользователей с возрастом 30";
+       DB::table('users')->where('id', 5)->delete();
+        return "Пользователь с id=5 удален";
     }
 }
