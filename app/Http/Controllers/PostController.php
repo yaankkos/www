91f58261af080786dabe4dd9d37f916ba40bd50f
yana <nuymaftp@prof.ru>
2026-04-22 00:15:22 -0700

    Задача 6

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,11 +8,8 @@ class PostController extends Controller
 {
     public function show()
     {
-                DB::enableQueryLog();
-        DB::table('posts')->where('id', '!=', 3)->get();
-        $logs = DB::getQueryLog();
-        $time = $logs[0]['time'];
-        dump("Запрос выполнен за {$time} мс");
+               $users = DB::table('users')->select('name', 'email')->get();
+        dump($users);
 
 
     }
