a85733a1393cb9e9db602172699a5b9337b0a9cb
yana <nuymaftp@prof.ru>
2026-04-22 00:27:47 -0700

    Задача 18-19

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,14 +8,7 @@ class PostController extends Controller
 {
     public function show()
     {
-      $users = DB::table('users')
-            ->where(function($query) {
-                $query->whereBetween('age', [20, 30]);
-            })
-            ->orWhere(function($query) {
-                $query->whereBetween('salary', [400, 800]);
-            })
-            ->get();
-        dump($users);
+        $user = DB::table('users')->where('id', 3)->first();
+dump($user);
     }
 }
