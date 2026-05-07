1f4a7a9c77946268a48975b8954a502bce384a08
yana <nuymaftp@prof.ru>
2026-05-07 01:44:53 -0700

    задача 26.1

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/post/new', 'PostController@newPost');
-Route::post('/post/new', 'PostController@newPost');
+Route::post('/post/new', 'PostController@newPost'); 
